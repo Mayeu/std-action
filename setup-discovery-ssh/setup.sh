36e2011e2a3cc55da56ab52e4885f863be98651c
Mayeu <matthieu@objkt.com>
2026-09-02 15:33:51 +0200

    chore: format repo

--- setup-discovery-ssh/setup.sh
+++ setup-discovery-ssh/setup.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 # Setup known_hosts
 SSH_KNOWN_HOSTS_FILE="$(mktemp)"
-printenv DISCOVERY_SSH_KNOWN_HOSTS_ENTRY > "$SSH_KNOWN_HOSTS_FILE"
+printenv DISCOVERY_SSH_KNOWN_HOSTS_ENTRY >"$SSH_KNOWN_HOSTS_FILE"
 
 host_name="$(echo "$DISCOVERY_SSH_KNOWN_HOSTS_ENTRY" | cut -d ' ' -f 1)"
 
@@ -23,27 +23,27 @@ EOF
 
 # Setup auth
 ssh_key_file="$(mktemp)"
-printenv DISCOVERY_SSH_KEY > "$ssh_key_file"
+printenv DISCOVERY_SSH_KEY >"$ssh_key_file"
 if ssh-keygen -y -f "$ssh_key_file" &>/dev/null; then
   # Start ssh agent
   eval $(ssh-agent)
   # Add ssh key to agent
   ssh-add -q "$ssh_key_file" && rm "$ssh_key_file"
   # Auth agent socket to ssh config
-  echo "IdentityAgent $SSH_AUTH_SOCK" >> "$SSH_CONFIG_FILE"
+  echo "IdentityAgent $SSH_AUTH_SOCK" >>"$SSH_CONFIG_FILE"
 else
-  echo -e >&2 \
-"Your SSH key is not a valid OpenSSH private key\n"\
-"This is likely caused by one of these issues:\n"\
-"* The key has been configured incorrectly in your workflow file.\n"\
-"* The workflow was triggered by an actor that has no access to\n"\
-"  the GitHub Action secret used for storing your SSH key.\n"\
-"  For example, Pull Requests originating from a fork of your\n"\
-"  repository can't access secrets."
+  echo -e \
+    "Your SSH key is not a valid OpenSSH private key\n""\
+This is likely caused by one of these issues:\n""\
+* The key has been configured incorrectly in your workflow file.\n""\
+* The workflow was triggered by an actor that has no access to\n""\
+  the GitHub Action secret used for storing your SSH key.\n""\
+  For example, Pull Requests originating from a fork of your\n""\
+  repository can't access secrets." >&2
   exit 1
 fi
 
 # Append ssh config to system config
 sudo mkdir -p /etc/ssh
 sudo touch /etc/ssh/ssh_config
-sudo tee -a /etc/ssh/ssh_config < "$SSH_CONFIG_FILE" >/dev/null
+sudo tee -a /etc/ssh/ssh_config <"$SSH_CONFIG_FILE" >/dev/null
